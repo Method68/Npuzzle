d5e591bb1cd2a221aabc37d522f76e55f073d4e6
gabba <gabba@debian.home>
2017-02-25 23:28:53 +0000

    new interface merge proprely

--- idastar.py
+++ idastar.py
@@ -17,22 +17,22 @@ def getNextStates (width, current):
         empty = (i, empty)
         break
 
-    if (empty[1] < (width - 1) and laststate != 'LEFT'):
+    if (empty[1] < (width - 1)):
         a = [i[:] for i in current]
         a[empty[0]][empty[1]], a[empty[0]][empty[1] + 1] = a[empty[0]][empty[1] + 1], a[empty[0]][empty[1]]
         nextStates.append((a, 'RIGHT'))
 
-    if (empty[1] > 0 and laststate != 'RIGHT'):
+    if (empty[1] > 0):
         b = [i[:] for i in current]
         b[empty[0]][empty[1]], b[empty[0]][empty[1] - 1] = b[empty[0]][empty[1] - 1], b[empty[0]][empty[1]]
         nextStates.append((b, 'LEFT'))
 
-    if (empty[0] > 0 and laststate != 'DOWN'):
+    if (empty[0] > 0):
         c = [i[:] for i in current]
         c[empty[0]][empty[1]], c[empty[0] - 1][empty[1]] = c[empty[0] - 1][empty[1]], c[empty[0]][empty [1]]
         nextStates.append((c, 'UP'))
 
-    if (empty[0] < (width - 1) and laststate != 'UP'):
+    if (empty[0] < (width - 1)):
         d = [i[:] for i in current]
         d[empty[0]][empty[1]], d[empty[0] + 1][empty [1]] = d[empty[0] + 1][empty[1]], d[empty[0]][empty [1]]
         nextStates.append((d, 'DOWN'))
